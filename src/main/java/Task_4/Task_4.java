d3f8003b769f7fce3fc485b776b2d4dfd63c62b5
Hazaza1488 <103219633+Hazaza1488@users.noreply.github.com>
2023-06-05 23:44:48 +0300

    massage

--- src/main/java/Task_4/Task_4.java
+++ src/main/java/Task_4/Task_4.java
@@ -1,104 +1,113 @@
-package Task_4;
+package Task4;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Task_4 {
-    public static void main(String[] args) throws InterruptedException {
+public class Task4 {
+    public static void main(String[] args) {
+        final Object lock = new Object();
 
-        // Create a thread that waits for a signal from another thread using wait and notify
-        Object lock = new Object();
-        Thread thread1 = new Thread(() -> {
+        Thread waitingThread = new Thread(() -> {
             synchronized (lock) {
                 try {
-                    System.out.println("Thread 1 is waiting...");
+                    System.out.println("Waiting for a signal...");
                     lock.wait();
-                    System.out.println("Thread 1 is notified.");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println("Signal received. Resuming execution.");
             }
         });
-        thread1.start();
 
-        Thread.sleep(1000); // Wait for 1 second
-
-        Thread thread2 = new Thread(() -> {
+        Thread signalingThread = new Thread(() -> {
             synchronized (lock) {
-                System.out.println("Thread 2 is notifying...");
+                System.out.println("Signal sent.");
                 lock.notify();
             }
         });
-        thread2.start();
 
-        // Create a custom class, modify its field value using reflection, and print out the object before and after changes
-        CustomClass customObject = new CustomClass("Hello", 123, true);
-        printFields(customObject);
-        modifyField(customObject, "field2", 456);
-        printFields(customObject);
+        waitingThread.start();
+        signalingThread.start();
 
-        // Create a generic class that can store a list of elements of any type and add elements to it using List<T> and add()
-        GenericClass<Integer> intList = new GenericClass<>();
-        intList.add(1);
-        intList.add(2);
-        intList.add(3);
-        System.out.println(intList.getList());
+        MyClass myObject = new MyClass();
+        myObject.number = 10;
+        myObject.text = "Hello";
 
-        GenericClass<String> stringList = new GenericClass<>();
-        stringList.add("a");
-        stringList.add("b");
-        stringList.add("c");
-        System.out.println(stringList.getList());
-    }
-
-    public static void printFields(Object object) {
-        Field[] fields = object.getClass().getDeclaredFields();
+        // Displaying fields and their types
+        Field[] fields = myObject.getClass().getDeclaredFields();
         for (Field field : fields) {
-            field.setAccessible(true);
-            try {
-                System.out.println(field.getName() + " - " + field.getType() + " : " + field.get(object));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Field name: " + field.getName());
+            System.out.println("Field type: " + field.getType());
         }
-    }
 
-    public static void modifyField(Object object, String fieldName, Object value) {
+        // Changing the value of a specific field
         try {
-            Field field = object.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(object, value);
+            Field textField = myObject.getClass().getDeclaredField("text");
+            textField.setAccessible(true);
+            textField.set(myObject, "Modified text");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
-    }
 
-    public static class CustomClass {
-        private String field1;
-        private int field2;
-        private boolean field3;
+        // Displaying the object before and after the changes
+        System.out.println("Object before changes: " + myObject.toString());
 
-        public CustomClass(String field1, int field2, boolean field3) {
-            this.field1 = field1;
-            this.field2 = field2;
-            this.field3 = field3;
+        try {
+            Field textField = myObject.getClass().getDeclaredField("text");
+            textField.setAccessible(true);
+            textField.set(myObject, "Modified text");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Object after changes: " + myObject.toString());
+
+        GenericClass<String> stringClass = new GenericClass<>();
+        stringClass.add("Element 1");
+        stringClass.add("Element 2");
+
+        List<String> stringElements = stringClass.getElements();
+        for (String element : stringElements) {
+            System.out.println(element);
+        }
+
+        GenericClass<Integer> integerClass = new GenericClass<>();
+        integerClass.add(1);
+        integerClass.add(2);
+
+        List<Integer> integerElements = integerClass.getElements();
+        for (Integer element : integerElements) {
+            System.out.println(element);
         }
     }
+}
 
-    public static class GenericClass<T> {
-        private List<T> list;
+class MyClass {
+    private int number;
+    private String text;
 
-        public GenericClass() {
-            list = new ArrayList<>();
-        }
+    @Override
+    public String toString() {
+        return "MyClass{" +
+                "number=" + number +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
 
-        public void add(T element) {
-            list.add(element);
-        }
+class GenericClass<T> {
+    private List<T> elements;
 
-        public List<T> getList() {
-            return list;
-        }
+    public GenericClass() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        elements.add(element);
+    }
+
+    public List<T> getElements() {
+        return elements;
     }
 }
