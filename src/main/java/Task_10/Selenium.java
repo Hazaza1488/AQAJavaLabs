71d613be7e10311e60f062ab9b05acec40ff04af
Hazaza1488 <103219633+Hazaza1488@users.noreply.github.com>
2023-06-10 14:10:36 +0300

    massage

---
+++ src/main/java/Task_10/Selenium.java
@@ -0,0 +1,39 @@
+package org.example.task_10;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumStart {
+    public static void main(String[] args) {
+
+        ChromeDriverManager.getInstance().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://demoqa.com/elements");
+
+        WebElement navItem = driver.findElement(By.xpath("//*[@id=\"itemc\"]"));
+        navItem.click();
+
+        WebElement img = driver.findElement(By.xpath("//*[@id=\"article\"]"));
+        System.out.println(img.getSize());
+        System.out.println(img.getAttribute("src"));
+
+        WebElement practiceForm = driver.findElement(By.xpath("//*[@id=\"carouselExampleIndicators\"]/a[2]/span[1]"));
+        practiceForm.click();
+
+        WebElement form = driver.findElement(By.xpath("//*[@id=\"prev2\"]"));
+        navItem.click();
+
+        WebElement form = driver.findElement(By.xpath("//*[@id=\"prev2\"]"));
+        navItem.click();
+
+        driver.close();
+        driver.quit();
+    }
+}
